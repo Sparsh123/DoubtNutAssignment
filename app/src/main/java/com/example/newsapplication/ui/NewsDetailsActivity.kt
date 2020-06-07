a362a4e6f68e8317a1a5fca393c1e1d328a772f5
Sparsh <sparsh.script@gmail.com>
2020-06-08 02:02:04 +0530

    Implementation of webView

--- app/src/main/java/com/example/newsapplication/ui/NewsDetailsActivity.kt
+++ app/src/main/java/com/example/newsapplication/ui/NewsDetailsActivity.kt
@@ -3,26 +3,81 @@ package com.tracki.ui.rides
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import com.example.newsapplication.R
+import com.example.newsapplication.data.Articles
 import com.example.newsapplication.data.NewsResponse
-import com.example.newsapplication.ui.base.BaseActivity
-import com.google.gson.Gson
-
-import java.util.*
-import javax.inject.Inject
 
 
 class NewsDetailsActivity :AppCompatActivity() {
 
-    var newsResponse:NewsResponse?=null
+    private var newsResponse:NewsResponse?=null
+    private var ivNewsDetails:ImageView?=null
+    private var tvSource:TextView?=null
+    private var tvAuthor:TextView?=null
+    private var tvTitle:TextView?=null
+    private var tvDetail:TextView?=null
+    private var tvPub:TextView?=null
+    private var webView:WebView?=null
+    private var mList:List<Articles>?=null
+    private var pos:String?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_details)
+//        ivNewsDetails = findViewById(R.id.ivNewsDetails)
+//        tvSource=findViewById(R.id.tvSource)
+//        tvAuthor= findViewById(R.id.tvAuthor)
+//        tvTitle = findViewById(R.id.tvTitle)
+//        tvDetail=findViewById(R.id.tvDetail)
+//        tvPub=findViewById(R.id.tvPub)
+        webView=findViewById(R.id.webView)
+        webView!!.setWebViewClient(object:WebViewClient() {
+
+            override fun onPageFinished(view:WebView, url:String) {
+
+
+                webView!!.scrollTo(100,100);
+                webView!!.setInitialScale(100);
+            }})
+        webView!!.requestFocusFromTouch();
+        webView!!.setVerticalScrollBarEnabled(true);
+        webView!!.setHorizontalScrollBarEnabled(true);
         val bundle = intent.extras
         newsResponse = bundle!!.getSerializable("NewsResponse") as NewsResponse
+        mList=newsResponse!!.articles
+        pos=bundle.getString("Pos")
+        setup()
+
+
+    }
+
+    fun setup()
+    {
+
+        if(mList!![pos!!.toInt()].urlToImage!=null) {
+            val uri: String = mList!![pos!!.toInt()].url!!
+            webView!!.getSettings().setJavaScriptEnabled(true);
+            webView!!.getSettings().setUseWideViewPort(true);
+            webView!!.getSettings().setLoadWithOverviewMode(true);
+            webView!!.getSettings().setBuiltInZoomControls(true);
+            webView!!.loadUrl(uri)
+
+        }
+
+//        tvSource!!.setText("Source: " + mList!![pos!!.toInt()].source)
+//        tvAuthor!!.setText("Author: " + mList!![pos!!.toInt()].author)
+//        tvTitle!!.setText(mList!![pos!!.toInt()].title.toString())
+//        tvDetail!!.setText(mList!![pos!!.toInt()].content.toString())
+//        var words:List<String>?=null
+//        words = mList!![pos!!.toInt()].publishedAt!!.split("T")
+//        tvPub!!.setText("Published at: " + words.get(0) + ", " + words.get(1) )
+
+
 
     }
 
@@ -33,3 +88,18 @@ class NewsDetailsActivity :AppCompatActivity() {
 
 
 }
+
+//class WebViewController : WebViewClient() {
+//    override fun shouldOverrideUrlLoading(
+//        view: WebView,
+//        url: String
+//    ): Boolean {
+//        view.loadUrl(url)
+//        return true
+//    }
+//
+//    override fun onPageFinished(view: WebView?, url: String?) {
+//        webView.scrollTo(100, 100)
+//        webView.setInitialScale(100)
+//    }
+//}
